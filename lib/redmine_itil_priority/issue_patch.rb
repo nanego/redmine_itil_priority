29d211fbe891ed9f0d5af9181e1e63baaf621d8e
Brice Beaumesnil <brice.beaumesnil@douane.finances.gouv.fr>
2022-02-25 10:20:04 +0100

    update priority from context-menu

--- lib/redmine_itil_priority/issue_patch.rb
+++ lib/redmine_itil_priority/issue_patch.rb
@@ -3,4 +3,24 @@ require_dependency 'issue'
 class Issue
   safe_attributes 'impact_id'
   safe_attributes 'urgency_id'
+
+  def urgency_id=(pid)
+    unless self.impact_id.nil?
+      if @settings.nil?
+        @settings = Setting['plugin_redmine_itil_priority']
+      end
+      write_attribute(:priority_id, @settings["priority_i" + self.impact_id.to_s + "_u" + pid])
+    end
+    write_attribute(:urgency_id, pid)
+  end
+
+  def impact_id=(pid)
+    unless self.urgency_id.nil?
+      if @settings.nil?
+        @settings = Setting['plugin_redmine_itil_priority']
+      end
+      write_attribute(:priority_id, @settings["priority_i" + pid + "_u" + self.urgency_id.to_s])
+    end
+    write_attribute(:impact_id, pid)
+  end
 end
